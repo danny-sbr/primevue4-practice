cc9bf16c0d2d6a8b758b51fcf406534e2e046dd3
danny_chen <danny_chen@seberon.com>
2025-01-20 18:18:11 +0800

    feat: update router configuration to include new components route with nested views for better structure

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,20 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/components',
+      children: [
+        {
+          path: '',
+          name: 'components',
+          component: () => import('../views/ComponentsView/ComponentsView.vue'),
+        },
+        {
+          path: 'menu',
+          name: 'menu',
+          component: () =>
+            import('../views/ComponentsView/MenuView/MenuView.vue'),
+        },
+      ],
     },
   ],
 })
