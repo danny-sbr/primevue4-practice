4cc3498546424bead73b6f541df41ec6798fd1a9
danny_chen <danny_chen@seberon.com>
2025-01-20 18:18:04 +0800

    feat: add CodeBlock component for syntax highlighting using PrismJS

---
+++ src/components/CodeBlock.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted } from 'vue'
+import Prism from 'prismjs'
+import 'prismjs/themes/prism.css'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
+
+onMounted(() => {
+  window.Prism = window.Prism || {}
+  window.Prism.manual = true
+  Prism.highlightAll()
+})
+</script>
+
+<template>
+  <div>
+    <pre><code class="language-javascript line-numbers"><slot /></code></pre>
+  </div>
+</template>
+
+<style scoped></style>
