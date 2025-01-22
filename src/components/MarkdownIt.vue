8eed795183003baa6d4ca4a6084709d77a9d3117
danny_chen <danny_chen@seberon.com>
2025-01-22 17:28:11 +0800

    feat: 增強 MarkdownIt 組件，新增行號高亮功能及改進程式碼區塊渲染，並整合 markdown-it-prism 插件以提升語法醒目提示效果

--- src/components/MarkdownIt.vue
+++ src/components/MarkdownIt.vue
@@ -14,9 +14,23 @@ import MarkdownItIns from 'markdown-it-ins'
 import MarkdownItMark from 'markdown-it-mark'
 import MarkdownItSub from 'markdown-it-sub'
 import MarkdownItSup from 'markdown-it-sup'
+import MarkdownItPrism from 'markdown-it-prism'
 
-// 建立 Markdown 解析器的實例並加載插件
-const markdown = new MarkdownIt()
+const lineNumbers = (originalHighlight) => {
+  return (text, lang) => {
+    const html = originalHighlight(text, lang)
+    // Similar to: https://github.com/11ty/eleventy-plugin-syntaxhighlight/blob/7b7b547fff07f2e60d91c0a7ed3bba1938dbc057/src/markdownSyntaxHighlightOptions.js#L28
+    const lines = html.split('\n').slice(0, -1)
+
+    return lines
+      .map((line) => `<span class="highlight-line">${line}</span>`)
+      .join('\n')
+  }
+}
+
+const markdown = new MarkdownIt({
+  html: true,
+})
   .use(MarkdownItAbbr)
   .use(MarkdownItContainer)
   .use(MarkdownItDeflist)
@@ -26,6 +40,19 @@ const markdown = new MarkdownIt()
   .use(MarkdownItMark)
   .use(MarkdownItSub)
   .use(MarkdownItSup)
+  .use(MarkdownItPrism)
+
+// https://github.com/jGleitz/markdown-it-prism/issues/1
+// https://github.com/andreas-mausch/eleventy-sample/blob/82cca7f7166cb8147d4681d0ba6e7b720f1889ac/eleventy/markdown.js
+// Unfortunately, inline code blocks are not processed by default, so we need this.
+// Compare to https://github.com/11ty/eleventy-plugin-syntaxhighlight/issues/38#issuecomment-1022305948
+
+markdown.renderer.rules.code_inline = (tokens, idx, { langPrefix = '' }) => {
+  const token = tokens[idx]
+  return `<code class="${langPrefix}">${token.content}</code>`
+}
+
+markdown.options.highlight = lineNumbers(markdown.options.highlight)
 
 // 從 props 獲取來源內容
 const props = defineProps({
@@ -37,3 +64,133 @@ const props = defineProps({
 
 const source = ref(props.source)
 </script>
+
+<style scoped>
+:deep(h1) {
+  font-size: 2em;
+  margin-bottom: 0.5em;
+  font-weight: bold;
+}
+
+:deep(h2) {
+  font-size: 1.5em;
+  margin-bottom: 0.5em;
+  font-weight: bold;
+}
+
+:deep(h3) {
+  font-size: 1.3em;
+  margin-bottom: 0.5em;
+}
+
+:deep(h4) {
+  font-size: 1.2em;
+  margin-bottom: 0.5em;
+  font-weight: bold;
+}
+
+:deep(h5) {
+  font-size: 1.1em;
+  margin-bottom: 0.5em;
+  font-weight: bold;
+}
+
+:deep(p) {
+  margin-bottom: 1em;
+  line-height: 1.6;
+}
+
+:deep(ul),
+:deep(ol) {
+  margin-bottom: 1em;
+  padding-left: 2em;
+}
+
+:deep(li) {
+  list-style-type: disc;
+}
+:deep(li) {
+  margin-bottom: 0.5em;
+}
+
+:deep(:not(pre) > code) {
+  padding: 2px 4px;
+  font-size: 90%;
+  color: #c7254e;
+  background-color: #f9f2f4;
+  border-radius: 4px;
+  font-family: monospace;
+}
+
+/* :deep(pre) {
+  background-color: #f5f5f5;
+  padding: 1em;
+  border-radius: 5px;
+  overflow-x: auto;
+  margin-bottom: 1em;
+} */
+
+:deep(blockquote) {
+  border-left: 4px solid #ddd;
+  padding-left: 1em;
+  margin-left: 0;
+  margin-bottom: 1em;
+  color: #666;
+}
+
+:deep(a) {
+  color: #0366d6;
+  text-decoration: none;
+}
+
+:deep(a:hover) {
+  text-decoration: underline;
+}
+
+:deep(table) {
+  border-collapse: collapse;
+  width: 100%;
+  margin-bottom: 1em;
+}
+
+:deep(th),
+:deep(td) {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+}
+
+:deep(th) {
+  background-color: #f5f5f5;
+}
+
+:deep(img) {
+  max-width: 100%;
+  height: auto;
+}
+
+:deep(hr) {
+  border: none;
+  border-top: 1px solid #ddd;
+  margin: 1em 0;
+}
+
+:deep(code[class*='language-']) {
+  counter-reset: lineNumber;
+  font-size: 9pt;
+}
+
+:deep(code[class*='language-'] .highlight-line::before) {
+  border-right: 1px solid #404040;
+  color: #858585;
+  content: counter(lineNumber);
+  counter-increment: lineNumber;
+  display: inline-block;
+  font-variant-numeric: tabular-nums;
+  margin-right: 0.6em;
+  padding-right: 2em;
+  text-align: right;
+  user-select: none;
+  width: 1.4em;
+}
+</style>
