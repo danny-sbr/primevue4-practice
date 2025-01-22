a6efb826f5febe331324a49c2dfd7a18f8d1c924
danny_chen <danny_chen@seberon.com>
2025-01-22 14:31:35 +0800

    feat: 新增 MarkdownIt 組件以支援 Markdown 解析，並整合多個 markdown-it 插件以增強功能

---
+++ src/components/MarkdownIt.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-html="markdown.render(source)" />
+</template>
+
+<script setup>
+import { defineProps, ref } from 'vue'
+import MarkdownIt from 'markdown-it'
+import MarkdownItAbbr from 'markdown-it-abbr'
+import MarkdownItContainer from 'markdown-it-container'
+import MarkdownItDeflist from 'markdown-it-deflist'
+import MarkdownItFootnote from 'markdown-it-footnote'
+import MarkdownItForInline from 'markdown-it-for-inline'
+import MarkdownItIns from 'markdown-it-ins'
+import MarkdownItMark from 'markdown-it-mark'
+import MarkdownItSub from 'markdown-it-sub'
+import MarkdownItSup from 'markdown-it-sup'
+
+// 建立 Markdown 解析器的實例並加載插件
+const markdown = new MarkdownIt()
+  .use(MarkdownItAbbr)
+  .use(MarkdownItContainer)
+  .use(MarkdownItDeflist)
+  .use(MarkdownItFootnote)
+  .use(MarkdownItForInline)
+  .use(MarkdownItIns)
+  .use(MarkdownItMark)
+  .use(MarkdownItSub)
+  .use(MarkdownItSup)
+
+// 從 props 獲取來源內容
+const props = defineProps({
+  source: {
+    type: String,
+    default: '',
+  },
+})
+
+const source = ref(props.source)
+</script>
