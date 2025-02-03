719038cf1472bd69b4ee9e3ec35dc3fa2a512d6a
danny_chen <danny_chen@seberon.com>
2025-02-03 17:02:28 +0800

    docs: Expand VitePress configuration with new documentation sections and sidebar structure

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "primevue4-practice",
-  description: "primevue4 practice description",
+  title: 'primevue4-practice-doc',
+  description: 'primevue4 practice description',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
     ],
 
     sidebar: [
@@ -16,13 +16,46 @@ export default defineConfig({
         text: 'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+        collapsed: true,
+      },
+      {
+        text: 'Getting Started',
+        items: [
+          {
+            text: 'Introduction',
+            link: '/primevue_doc/GettingStarted/Introduction',
+          },
+        ],
+      },
+      {
+        text: 'Configuration',
+        items: [
+          { text: 'Options', link: '/primevue_doc/Configuration/Options' },
+          {
+            text: 'AutoImport',
+            link: '/primevue_doc/Configuration/AutoImport',
+          },
+        ],
+      },
+      {
+        text: 'Theming',
+        items: [
+          { text: 'StyledMode', link: '/primevue_doc/Theming/StyledMode' },
+        ],
+      },
+      {
+        text: 'PassThrough',
+        link: '/primevue_doc/PassThrough/PassThrough',
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+    markdown: {
+      lineNumbers: true,
+    },
+  },
 })
