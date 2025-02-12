93ecdad3cd5836f47d04c7df548d864456e6deb9
danny_chen <danny_chen@seberon.com>
2025-02-12 17:17:13 +0800

    chore: add ignore ts nocheck

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+// @ts-nocheck
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [import('tailwindcss-primeui')],
+  plugins: [require('tailwindcss-primeui')],
 }
