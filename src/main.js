37aafe035abc0376d8c3ebc06866a54e10742be0
danny_chen <danny_chen@seberon.com>
2025-02-12 17:17:39 +0800

    feat: Customize PrimeVue Aura theme with cyan color palette

--- src/main.js
+++ src/main.js
@@ -4,11 +4,30 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+import { definePreset } from '@primevue/themes'
 
 import Aura from '@primevue/themes/aura'
 
 import App from './App.vue'
 import router from './router'
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{cyan.50}',
+      100: '{cyan.100}',
+      200: '{cyan.200}',
+      300: '{cyan.300}',
+      400: '{cyan.400}',
+      500: '{cyan.500}',
+      600: '{cyan.600}',
+      700: '{cyan.700}',
+      800: '{cyan.800}',
+      900: '{cyan.900}',
+      950: '{cyan.950}',
+    },
+  },
+})
 
 const app = createApp(App)
 
@@ -16,7 +35,7 @@ app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: MyPreset,
     options: {
       prefix: 'p',
       darkModeSelector: false,
